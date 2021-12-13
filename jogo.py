c695ddfed838ca8961d87965907bf8fef9ed46f4
leticiafelippe <leticiafelippes@gmail.com>
2021-12-13 16:17:05 -0300

    algoritmo em teste de multiplayer :bug:

---
+++ jogo.py
@@ -0,0 +1,26 @@
+jogadores = ['a', 'b', 'c']
+
+listaDePalavras = []
+
+def palavra():
+    letra = input(f'{i}, é a sua vez. Digite uma palavra: '
+
+
+while True:
+    for i in jogadores:
+        while True:
+            palavra()
+            if letra not in list.(listaDePalavras):
+                print('Você errou')
+            else:
+                print ('Você acertou')
+                palavra()
+                
+
+
+#######################
+palavra_do_jogador = []
+
+for i in range(len(jogadores)):
+palavra_do_jogador[i]
+jogador[i]
